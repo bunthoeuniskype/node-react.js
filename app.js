7655c3bbba28acc4453b2b3ffa03cefc49e08d1e
bunthoeun <bunthoeunmake@gmail.com>
2020-11-07 00:01:09 +0700

    node mvc

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require("body-parser");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var customersRouter = require('./routes/customer.routes');
 
 var app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', customersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ views/error.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+
+function Error(props) {
+  return <div>Looking something wrong</div>;
+}
+
+module.exports = Error;
